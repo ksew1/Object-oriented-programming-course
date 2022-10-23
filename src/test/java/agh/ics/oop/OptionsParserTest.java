7072d51d05e50ba43f32cd55f1cd09ccef44d96c
ksew1 <karol.sewilo@gmail.com>
2022-10-23 22:24:37 +0200

    lab3

--- src/test/java/agh/ics/oop/OptionsParserTest.java
+++ src/test/java/agh/ics/oop/OptionsParserTest.java
@@ -3,6 +3,17 @@ package agh.ics.oop;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
+/* Odpowiedz na pytanie: jak zaimplementować mechanizm, który wyklucza pojawienie się
+dwóch zwierząt w tym samym miejscu.
+
+Odpowiedź:
+Utworzyłbym, klasę Map. Posiadała by ona boolowksą tablicę 4 na 4.
+Jeśli zwierzak już jest na damym polu było by one wypełnione true. W przeciwnym przypadku false.
+Zaimplementował bym 3 metody. Przyjmowały by one współrzędne danego pola. Pierwsza metoda by na
+podstawie współrzędnych zmieniała wartość na false, druga na true, a ostania metoda zwracała wartość
+danego pola. Jeśli zwierzak by się pojawił na damym polu to z mieniamy wartość na true. Jeśli
+Schodzi z niego to false. Przed wejściem zwierzaka sprawdzalibyśmy czy pole ma wartość false. Jeśli
+tak to wchodzimy jeśli nie to nie. Złożoność O(1) */
 public class OptionsParserTest {
     @Test
     public void parseTest() {
